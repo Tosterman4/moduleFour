c3262926260fb90646306309ddd1e145f52a7225
Tosterman4 <Rinat_parradzhov@mail.ru>
2024-05-26 23:09:55 +0500

    Создан цикл на вывод перевернутого массива из задания 4.3.7

--- moduleFour/Program.cs
+++ moduleFour/Program.cs
@@ -10,40 +10,67 @@ namespace moduleFour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-            var color = Console.ReadLine();
-
-            switch (color)
+            //Задание по циклам
+            /*Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            int t = 0;
+            do
             {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
+                var text = Console.ReadLine();
+                if (text == "stop")
+                {
+                    Console.WriteLine("is stoped");
                     break;
+                }
+                switch (text)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is red!");
+                        break;
 
-                    Console.WriteLine("Your color is green!");
-                    break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your color is green!");
+                        break;
 
-                    Console.WriteLine("Your color is cyan!");
-                    break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
 
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color is cyan!");
+                        break;
 
-                    Console.WriteLine("Your color is yellow!");
-                    break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+                t++;
             }
+            while (t < 3);
+            */
+
+            //Задание по массивам
+
+            Console.WriteLine("Введите своё имя");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя по буквам: ");
+
+            for (var ch=name.Length-1;ch>-1;ch--)
+            {
+                Console.Write(name[ch] + " ");
+            }
+
+            Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+
             Console.ReadKey();
         }
     }
